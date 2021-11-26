18508554254af8bea98744ada43a8961a26bfaab
dev-sda1 <cookinggaga@outlook.com>
2021-11-26 17:03:48 +0000

    Reverting to coming soon ident

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import '../styles/globals.css'
-import NavBar from '../modules/navbar.js'
-import Footer from '../modules/footer.js'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="app">
-      <NavBar/>
-      <Component {...pageProps} />
-      {/* <Footer/> */}
-    </div>
-    
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import navbarStyle from '../styles/Navbar.module.css'
-import NavBar from '../modules/navbar.js'
-import Footer from '../modules/footer.js'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Brunel Tech Society</title>
-        <meta name="description" content="The website for Brunel Techsoc" />
+        <meta name="description" content="Website Under Construction." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        {/* Header */}
-        {/* <NavBar /> */}
-
-        {/* Main Content*/}
-        <div className={styles.mainContent}>
-          {/* Add a large, dark image to the background, with text infront and a down arrow to scroll the page */}
-          <div className={styles.imageContainer}>
-            <div className={styles.textContainer}>
-              <h1 className={styles.title}>something to go here (including a decent image)</h1>
-              <a href="#about" className={styles.scrollDown}>
-                <img src="" alt="Click to Scroll Down" />
-              </a>
-            </div>
-          </div>
-
-          <div className={styles.content_intro}>
-
-          </div>
-        </div>
+        <Image src="/ident.png" alt="Brunel Tech Society" width={200} height={200} />
+        <h1 className={styles.title}>Under Construction</h1>
       </main>
-
-      {/* <Footer/> */}
-      <Footer/>
     </div>
   )
 }
