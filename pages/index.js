485a226ba77d93ebc45d63efb3fad482b0e03968
dev-sda1 <cookinggaga@outlook.com>
2021-12-02 22:45:39 +0000

    Added some more info

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,23 @@ export default function Home() {
         <title>Brunel Tech Society</title>
         <meta name="description" content="Website Under Construction." />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/fba0knw.css"></link>
       </Head>
 
       <main className={styles.main}>
-        <Image src="/ident.png" alt="Brunel Tech Society" width={200} height={200} />
+        <img src="ts_kind_of_web_friendly.png" alt="Brunel Tech Society" className={styles.logo_large}/>
+        <img src="small_logo.png" alt="Brunel Tech Society - Mobile Logo" className={styles.logo_small}/>
+
         <h1 className={styles.title}>Under Construction</h1>
+        <h4 className={styles.title_sub}>(watch this space)</h4>
+
+        <a href="https://brunelstudents.com/societies/tech"><div className={styles.button}>
+          <p>Learn More</p>
+        </div></a>
+        <br></br>
+        <a href="mailto:info@bruneltech.net"><div className={styles.button}>
+          <p>Get in Touch</p>
+        </div></a>
       </main>
     </div>
   )
